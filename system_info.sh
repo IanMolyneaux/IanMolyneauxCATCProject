3d857b4fb1df45c940af129bdc934db491c3c367
IanMolyneaux <ianmolyneaux92@gmail.com>
2025-04-27 18:05:49 +0000

    Created system_info.sh file and added the required commands. Still need to test!

---
+++ system_info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#Script to gather system information
+#
+#echo "Current Date and Time:"
+#date
+#
+#echo "Hostname:"
+#hostname
+#
+#echo "System Architecture:"
+#arch
+#
+#echo "Kernel and OS details:"
+#uname -a
+#
+#echo "System Uptime:"
+#uptime
+#
+#echo "Current User:"
+#whoami
+#
+#echo "Who is logged in:"
+#who
+#
+#echo "Current Activity:"
+#w
+#
+#echo "Table of Processes (press q to quit):"
+#top
+#
+#echo "Command History:"
+#history
