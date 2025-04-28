a3f75ded81a95fa8f2a1bbafe60139fd47be5162
IanMolyneaux <ianmolyneaux92@gmail.com>
2025-04-28 14:41:48 +0000

    Added salary scale calculation loop

--- Q2.4_salary_scale_table.sh
+++ Q2.4_salary_scale_table.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#Q2.2_salary_scale_table.sh
-#Author: Ian Molyneaux
-#Date: 28/04/2025
-#Description: Generate a salary scale based on user input
+# Q2.2_salary_scale_table.sh
+# Author: Ian Molyneaux
+# Date: 28/04/2025
+# Description: Generate a salary scale based on user input
 
-#User input Starting salary
+# User input Starting salary
 echo "Enter starting salary:"
 read starting_salary
 echo "Starting Salary: $starting_salary"
 
-#Points on salary scale and salary increment
+# Points on salary scale and salary increment
 echo "Enter number of points on salary scale:"
 read number_of_points
 
@@ -20,9 +20,24 @@ read salary_increment
 echo "Number of Points: $number_of_points"
 echo "Salary Increment: $salary_increment"
 
-#Check if employee is a Manager
+# Check if employee is a Manager
 echo "Is the employee a manager? (yes/no):"
 read is_manager
 
-#Getting the current year
-current_year=$(date +%Y)
+# Getting the current year
+current_year = $(date +%Y)
+
+# Loop for salary scale calculation
+salary = $starting_salary
+year = $current_year
+point = 1
+
+echo "Year | Salary"
+
+while [ $point -le $number_of_points ]
+do
+	echo "$year | $salary"
+	salary = $(($salary + $salary_increment))
+	point = $(($point + 1))
+	year = $((year + 1))
+done
