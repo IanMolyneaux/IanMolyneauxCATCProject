96c630809f2739213fa9926637b9a7d723bb1dcb
IanMolyneaux <ianmolyneaux92@gmail.com>
2025-04-28 13:51:14 +0000

    Added user prompt and input for starting salary. Ensured this worked before commiting by making the script executable and running it

--- Q2.4_salary_scale_table.sh
+++ Q2.4_salary_scale_table.sh
@@ -4,3 +4,7 @@
 #Author: Ian Molyneaux
 #Date: 28/04/2025
 #Description: Generate a salary scale based on user input
+
+echo "Enter starting salary:"
+read starting_salary
+echo "Starting Salary: $starting_salary"
