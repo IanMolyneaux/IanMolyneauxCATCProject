df36fb3e70bbdc5cc73e477afa9fcd80e924ac0f
IanMolyneaux <ianmolyneaux92@gmail.com>
2025-04-28 14:00:51 +0000

    Added prompts for user input of number of points and salary increment to script

--- Q2.4_salary_scale_table.sh
+++ Q2.4_salary_scale_table.sh
@@ -5,6 +5,17 @@
 #Date: 28/04/2025
 #Description: Generate a salary scale based on user input
 
+#User input Starting salary
 echo "Enter starting salary:"
 read starting_salary
 echo "Starting Salary: $starting_salary"
+
+#Points on salary scale and salary increment
+echo "Enter number of points on salary scale:"
+read number_of_points
+
+echo "Enter salary increment:"
+read salary_increment
+
+echo "Number of Points: $number_of_points"
+echo "Salary Increment: $salary_increment"
